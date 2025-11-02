2f06cf89809e98f0e37d0eff3662c626c9f77b73
Sidharth Jain <sidharthjainp9@gmail.com>
2025-11-02 17:28:46 +0530

    2-cart-sec-7-8-addToCart,Total&Tax,emptyCart,ProductAddedToCArt,NavbarCheckoutIcon set

--- carts/models.py
+++ carts/models.py
@@ -15,6 +15,8 @@ class CartItem(models.Model):
     qty = models.IntegerField()
     is_active = models.BooleanField(default=True)
 
+    def sub_total(self):
+        return self.product.price * self.qty
 
     def __str__(self):
         return self.product
