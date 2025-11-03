3bbbdb62c988053f5c3a52d45a396d29e7814cbf
Sidharth Jain <sidharthjainp9@gmail.com>
2025-11-03 22:54:18 +0530

    4-carts-search function implemented

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 
 from store.models import Product
 from category.models import Category
@@ -49,3 +50,17 @@ def product_detail(request, category_slug, product_slug):
         'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+            context = {
+                'products': products,
+                'product_count': product_count,
+            }
+            return render(request, 'store/store.html', context)
+    return render(request, 'store/store.html')
