41c503511705667b1153e040966c6f621d68335a
Sidharth Jain <sidharthjainp9@gmail.com>
2025-11-03 14:10:34 +0530

    3-carts-sec-8-9-productAddedToCart,Paginator set

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from store.models import Product
 from category.models import Category
@@ -16,14 +17,20 @@ def store(request, category_slug=None):
         categories = get_object_or_404(Category, slug=category_slug)
         #queryset
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
         #queryset
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        'products' : products,
+        'products' : paged_products,
         'product_count' : product_count,
     }
     return render(request, 'store/store.html', context)
