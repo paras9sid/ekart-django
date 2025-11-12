71a14a36a5e2c1482e7eb42eb857463c72c0a71c
Sidharth Jain <sidharthjainp9@gmail.com>
2025-11-12 20:45:28 +0530

    3-dashbaord created and url set

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'), 
-    path('logout/', views.logout, name='logout'),     
+    path('logout/', views.logout, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
 
     #activate url for user registation activation
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
