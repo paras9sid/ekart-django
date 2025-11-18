2fb0babdc51b8e4c24a1d2d7ca00ead343d6ade5
Sidharth Jain <sidharthjainp9@gmail.com>
2025-11-18 16:52:36 +0530

    6-order page , order payment page reviewed

--- orders/views.py
+++ orders/views.py
@@ -8,6 +8,10 @@ from .models import Order
 
 
 # Create your views here.
+def payments(request):
+    return render(request, 'orders/payments.html')
+
+
 def place_order(request,total=0, qty=0):
     
     current_user = request.user
@@ -57,7 +61,18 @@ def place_order(request,total=0, qty=0):
             order_number = current_date + str(data.id) # <- convert id int into str format for concatenation w/o error
             data.order_number = order_number            
             data.save()
-            return redirect('checkout')
+
+            #order payment page
+            order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
+            context = {
+                'order': order,
+                'cart_items': cart_items,
+                'total': total,
+                'tax': tax,
+                'grand_total': grand_total,
+            }
+            return render(request, 'orders/payments.html', context)
     else:
         print("in else block error")
         return redirect('checkout')
+
